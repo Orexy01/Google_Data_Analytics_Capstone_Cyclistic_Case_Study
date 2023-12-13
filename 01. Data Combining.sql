4580921b0b31d1ae2d7290317a33a3735a4fee53
Folorunsho Oreoluwa <110780775+git-oreoluwa@users.noreply.github.com>
2023-12-13 15:07:23 +0100

    Update 01. Data Combining.sql

--- 01. Data Combining.sql
+++ 01. Data Combining.sql
@@ -1,27 +1,50 @@
-/* I had to work with 6 previous months containing data from Jan 2023 to Jun 2023 */
 
 -- Data Combining
 
--- combining all 6 months' data tables into a single table containing data from Jan 2023 to Jun 2023.
+-- combining all 12 months' data tables into a single table containing data from Jul 2022 to Jun 2023.
 
-CREATE TABLE IF NOT EXISTS `mainproject-393509.bike_dataset.tripdata_combined_data` AS (
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202301`
+CREATE TABLE IF NOT EXISTS `mainproject-393509.bike_dataset.tripdata-combined-data` AS (
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202207-01`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202302`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202207-02`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202303`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202207-03`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202304`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202208-01`
 UNION ALL 
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202305_01`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202208-02`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202305_02`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202209-01`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202306_01`
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202209-02`
 UNION ALL
-SELECT * FROM `mainproject-393509.bike_dataset.tripdata_202306_02`);
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202210-01`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202210-02`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202211`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202212`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202301`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202302`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202303`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202304`
+UNION ALL 
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202305-01`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202305-02`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202306-01`
+UNION ALL
+SELECT * FROM `mainproject-393509.bike_dataset.tripdata-202306-02`);
 
--- checking the number of rows, which is 2,390,459
+
+-- checking the number of rows, which is 5,779,433
 
 SELECT count(*)
-FROM `mainproject-393509.bike_dataset.tripdata_combined_data`;
+FROM `mainproject-393509.bike_dataset.tripdata-combined-data`;
+
